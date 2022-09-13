99e89e64387f08cb420249f2dbe419dd942b1188
ilya <lebedev-github@gmail.com>
2022-09-13 20:01:33 +0900

    fixed bags after isort

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,8 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-from django.shortcuts import get_object_or_404
 from rest_framework.validators import UniqueTogetherValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
